4160608284c3078418b32637125929ea715efcd5
Hanchenli <61769611+Hanchenli@users.noreply.github.com>
2025-11-16 23:10:30 -0800

    Refactor main function and add TPU worker message

--- diwali.py
+++ diwali.py
@@ -1,14 +1,9 @@
 from torch_xla import launch as xla_launch
 from torch_xla.runtime import world_size as xla_world_size
 
-
-def main():
-    print("Hanchen was here!")
-    print(xla_world_size())
-
-
 def xla_main(*args):
-    main()
+    print("Hello from a TPU worker!")
+    print("Global world size:", xla_world_size())
 
-
-xla_launch(xla_main)
+if __name__ == "__main__":
+    xla_launch(xla_main, nprocs=1)
