2d58a3db3f7d106e80a1b7d1e5891e17ad934007
Hanchenli <61769611+Hanchenli@users.noreply.github.com>
2025-11-16 23:10:56 -0800

    Change greeting message to 'Happy Diwali!'

--- diwali.py
+++ diwali.py
@@ -2,7 +2,7 @@ from torch_xla import launch as xla_launch
 from torch_xla.runtime import world_size as xla_world_size
 
 def xla_main(*args):
-    print("Hello from a TPU worker!")
+    print("Happy Diwali!")
     print("Global world size:", xla_world_size())
 
 if __name__ == "__main__":
